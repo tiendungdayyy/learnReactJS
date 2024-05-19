95767757746ac97032005bb98c056392a107f1c0
tiendungdayyy <91593609+tiendungdayyy@users.noreply.github.com>
2024-05-19 16:15:46 +0700

    homeWork

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,14 @@
-
+import MyComponent from "./baiTap/MyComponent";
 
 function App() {
   return (
     <div className="App">
         Hello World
+        <MyComponent></MyComponent>
+        <MyComponent></MyComponent>
+        <MyComponent></MyComponent>
+        <MyComponent></MyComponent>
+        <MyComponent></MyComponent>
     </div>
   );
 }
---
+++ my-app/src/baiTap/MyComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MyComponent extends React.Component{
+
+    render(){
+        return (
+            <div>Hello MyComponent</div>
+        )
+    }
+
+}
+
+export default MyComponent;
