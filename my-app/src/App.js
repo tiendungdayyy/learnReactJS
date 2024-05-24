43c5d9fffbca2d8a084c53216d2b2cb38ee00355
tiendungdayyy <91593609+tiendungdayyy@users.noreply.github.com>
2024-05-24 15:01:24 +0700

    day 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,13 @@
+import Day2 from "./Day2/Day2";
+import Button from "./Day2/Day2";
 import MyComponent from "./baiTap/MyComponent";
 
 function App() {
   return (
-    <div className="App">
-        Hello World
+    <>
         <MyComponent></MyComponent>
-        <MyComponent></MyComponent>
-        <MyComponent></MyComponent>
-        <MyComponent></MyComponent>
-        <MyComponent></MyComponent>
-    </div>
+        <Button></Button>
+      </>
   );
 }
 
